7aee4e8a3fd8501e0d8d15d3bd114dae5f3e5276
Lohith400 <lohiths092003@gmail.com>
2026-01-15 19:25:17 +0530

    changed at 7.24 15-01-26

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
+import React from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+
 import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
+    <Navbar/>
     <div className="App">
-      <h1>Lohith S</h1>
+        <h1>Lohith S</h1>
     </div>
+    <Footer/>
+    </BrowserRouter>
   );
 }
 
